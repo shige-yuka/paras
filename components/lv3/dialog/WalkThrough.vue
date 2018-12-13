2720d79ae4937ec0af7b8de215918d2d121b2d34
shige-yuka <shige.yuka.p@gmail.com>
2018-12-13 19:38:14 +0900

    walk through のスタイルを無理矢理変えた

--- components/lv3/dialog/WalkThrough.vue
+++ components/lv3/dialog/WalkThrough.vue
@@ -1,20 +1,20 @@
 <template>
   <v-dialog v-model="dialog" max-width="400px" :class="$style.dialog">
     <v-card :class="$style.dialog">
-      <v-carousel :cycle="false" light height="450">
+      <v-carousel :class="$style.carousel" :cycle="false" light height="450">
         <v-carousel-item>
           <div :class="$style.tentative"></div>
           <h2 :class="$style.title">やりたい事をメモしましょう。</h2>
-          <p :class="$style.text">頭の中にある「やりたい事」をメモしましょう。やりたい事が決まっていなくても大丈夫。日々が楽しくなる様な何かをAIが提案します。</p>
+          <p :class="$style.text">頭の中にある「やりたい事」をメモしましょう。やりたい事が決まっていなくても大丈夫。日々が楽しくなる様な何かをParasが提案します。</p>
         </v-carousel-item>
         <v-carousel-item>
           <div :class="$style.tentative"></div>
           <h2 :class="$style.title">マイルストーンで達成度をチェック</h2>
-          <p :class="$style.text">達成したらチェックマークを、つまづいたらコメントを残しましょう。あなたと同じチャレンジをしている人が助けてくれるかもしれません。</p>
+          <p :class="$style.text">達成したらチェックマークを、つまずいたらコメントを残しましょう。あなたと同じチャレンジをしている人が助けてくれるかもしれません。</p>
         </v-carousel-item>
         <v-carousel-item>
           <div :class="$style.tentative"></div>
-          <h2 :class="$style.title">つまづいている人におせっかい。</h2>
+          <h2 :class="$style.title">つまずいている人におせっかい。</h2>
           <p :class="$style.text">声をかけあったり、教科書を作ったり、先生として教えたり、仕送りしたり。誰かのチャレンジを応援しましょう。</p>
         </v-carousel-item>
       </v-carousel>
@@ -62,13 +62,10 @@ export default {
 }
 .startButtonWrap {
   width: 100%;
-  padding-top: var(--default);
+  padding: var(--narrow) 0 var(--default);
   text-align: center;
 }
-</style>
-
-<style scoped>
-.v-carousel__controls {
-  background: none !important;
+.carousel {
+  box-shadow: none;
 }
 </style>
