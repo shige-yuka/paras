a38ce12bf6304dceb11cf38c57d34bb6a05b9a50
shige-yuka <shige.yuka.p@gmail.com>
2019-01-09 00:16:41 +0900

    確認用に読み込ませたコンポーネントの記述を削除

--- src/pages/signup.vue
+++ src/pages/signup.vue
@@ -1,19 +1,16 @@
 <template>
   <article>
     サインアップー
-    <cheering-dialog />
   </article>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
-import CheeringDialog from '~/components/lv3/dialog/Cheering.vue'
 
 export default Vue.extend({
   layout: 'default',
 
   components: {
-    CheeringDialog,
   }
 })
 </script>
