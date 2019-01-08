56c6d341faf056bc2eb169b86d1121c06e0c501e
shige-yuka <shige.yuka.p@gmail.com>
2019-01-09 00:15:16 +0900

    仮に使ってないサインアップページに読み込ませる

--- src/pages/signup.vue
+++ src/pages/signup.vue
@@ -1,17 +1,19 @@
 <template>
   <article>
     サインアップー
+    <cheering-dialog />
   </article>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
-import HeroImage from '~/components/lv3/heroimage/Lp.vue'
+import CheeringDialog from '~/components/lv3/dialog/Cheering.vue'
 
 export default Vue.extend({
   layout: 'default',
 
   components: {
+    CheeringDialog,
   }
 })
 </script>
