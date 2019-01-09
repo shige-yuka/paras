4a670b0a184609ad32189b1a4a63e4c3ed8975b8
shige-yuka <shige.yuka.p@gmail.com>
2019-01-10 01:34:11 +0900

    開発予定のページをcomming soonにする

---
+++ src/pages/user/message.vue
@@ -0,0 +1,39 @@
+<template>
+  <article>
+    <section :class="$style.attainments">
+    </section>
+  </article>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import 'vuetify/dist/vuetify.min.css'
+export default Vue.extend({
+  layout: 'user',
+
+})
+</script>
+
+<style module>
+.attainments {
+  position: relative;
+  height: calc(100vh - 48px - 42px - 42px);
+  padding: var(--wide) var(--default) var(--default);
+}
+.attainments::before {
+  content: "coming soon!";
+  position: absolute;
+  top: 0;
+  left: 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  height: 100%;
+  background: var(--gray-light);
+  opacity: .8;
+  font-size: 4rem;
+  color: var(--gray-lighter);
+  z-index: 10;
+}
+</style>
